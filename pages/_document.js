70b0dfb6157c672c5690dbf55369195afba159b3
Priya-81199 <priya81199@gmail.com>
2021-11-21 17:39:20 +0530

    initial commit

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="shortcut icon" href="/img/brand/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/img/brand/apple-icon.png"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body className="text-blueGray-700 antialiased">
+          <div id="page-transition"></div>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.js
@@ -0,0 +1,171 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from "react";
+import Link from "next/link";
+
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import Footer from "components/Footers/Footer.js";
+
+export default function Index() {
+  return (
+    <>
+      <IndexNavbar fixed />
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <div className="container mx-auto items-center flex flex-wrap">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0">
+              <h1 className="font-semibold text-5xl text-blueGray-600">
+                Indie - Niche For Every Individual.
+              </h1>
+              <p className="mt-4 text-2xl leading-relaxed text-blueGray-500" style={{fontFamily: "Dancing Script, cursive"}}>
+              Indie is a place where you may read, write, and socialise.<br/>
+              It is and always will be free.
+              </p>
+              <div className="mt-12">
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus?ref=nnjs-index"
+                  target="_blank"
+                  className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-400 active:bg-blueGray-500 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                >
+                  Start Blogging
+                </a>
+                <a
+                  href="https://github.com/creativetimofficial/notus-nextjs?ref=nnjs-index"
+                  className="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg"
+                  target="_blank"
+                >
+                  Read Articles
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
+          src="/img/pattern_nextjs.png"
+          alt="..."
+        />
+      </section>
+
+      {/* <section className="block relative z-1 bg-blueGray-600">
+        <div className="container mx-auto">
+          <div className="justify-center flex flex-wrap">
+            <div className="w-full lg:w-12/12 px-4  -mt-24">
+              <div className="flex flex-wrap">
+                <div className="w-full lg:w-4/12 px-4">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Login Page
+                  </h5>
+                  <Link href="/auth/login">
+                    <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                      <img
+                        alt="..."
+                        className="align-middle border-none max-w-full h-auto rounded-lg"
+                        src="/img/login.jpg"
+                      />
+                    </div>
+                  </Link>
+                </div>
+
+                <div className="w-full lg:w-4/12 px-4">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Profile Page
+                  </h5>
+                  <Link href="/profile">
+                    <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                      <img
+                        alt="..."
+                        className="align-middle border-none max-w-full h-auto rounded-lg"
+                        src="/img/profile.jpg"
+                      />
+                    </div>
+                  </Link>
+                </div>
+
+                <div className="w-full lg:w-4/12 px-4">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Landing Page
+                  </h5>
+                  <Link href="/landing">
+                    <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                      <img
+                        alt="..."
+                        className="align-middle border-none max-w-full h-auto rounded-lg"
+                        src="/img/landing.jpg"
+                      />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section> */}
+      <section>
+        <div className="h-350-px">
+
+        </div>
+      </section>
+
+      <section className="pb-16 bg-blueGray-200 relative pt-32">
+        <div
+          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
+          style={{ transform: "translateZ(0)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-blueGray-200 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+
+        {/* <div className="container mx-auto">
+          <div className="flex flex-wrap justify-center bg-white shadow-xl rounded-lg -mt-64 py-16 px-12 relative z-10">
+            <div className="w-full text-center lg:w-8/12">
+              <p className="text-4xl text-center">
+                <span role="img" aria-label="love">
+                  😍
+                </span>
+              </p>
+              <h3 className="font-semibold text-3xl">
+                Do you love this Starter Kit?
+              </h3>
+              <p className="text-blueGray-500 text-lg leading-relaxed mt-4 mb-4">
+                Cause if you do, it can be yours now. Hit the buttons below to
+                navigate to get the Free version for your next project. Build a
+                new web app or give an old project a new look!
+              </p>
+              <div className="sm:block flex flex-col mt-10">
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus?ref=nnjs-index"
+                  target="_blank"
+                  className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-2 bg-blueGray-400 active:bg-blueGray-500 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                >
+                  Get started
+                </a>
+                <a
+                  href="https://github.com/creativetimofficial/notus-nextjs?ref=nnjs-index"
+                  target="_blank"
+                  className="github-star sm:ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg"
+                >
+                  <i className="fab fa-github text-lg mr-1"></i>
+                  <span>Help With a Star</span>
+                </a>
+              </div>
+              <div className="text-center mt-16"></div>
+            </div>
+          </div>
+        </div> */}
+      </section>
+      <Footer />
+    </>
+  );
+}
